62058be7097571c67580fd567c1bc42d9bc42486
James Houck <jameshouck@live.com>
2019-09-05 18:50:18 -0500

    Added banner to program main.

--- camelcase.py
+++ camelcase.py
@@ -2,13 +2,19 @@ def camelcase(sentence):
 	""" Convert sentence to camelCase, for example, "Display all books" 
 	is converted to "displayAllBooks" """
 
-title_case = sentence.title() # Uppercase first letter of each word
-upper_camel_cased = title_case.replace(' ', '') # remove spaces 
-# Lowercase first letter, join with rest of string 
-# Slices don't produce index out of bounds errors. 
-# So this still works on empty strings, strings of length 1
-return upper_camel_cased[0:1].lower() + upper_camel_cased[1:] 
-	def main():
+	title_case = sentence.title() # Uppercase first letter of each word
+	upper_camel_cased = title_case.replace(' ', '') # remove spaces 
+	# Lowercase first letter, join with rest of string 
+	# Slices don't produce index out of bounds errors. 
+	# So this still works on empty strings, strings of length 1
+
+	return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
+
+def banner():
+	print("AMAZING CAMELCASE PROGRAM")	 
+
+def main():
+	banner()
 	sentence = input('Enter your sentence: ')
 	output = camelcase(sentence)
 	print(output)
